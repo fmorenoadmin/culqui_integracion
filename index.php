5e203031283670680d8d05e343ee939245c8fe32
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2019-10-26 12:16:54 -0500

    Update index.php
    Arreglo de los errores

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <html>
 <head>
 	<title>Integración de Culqui</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 	<!-- Incluyendo Culqi Checkout -->
 	<script type="text/javascript" src="https://checkout.culqi.com/js/v3"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 	<!-- Google Fonts -->
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic,700,700italic,800' rel='stylesheet' type='text/css'>
@@ -23,10 +24,15 @@
 	?>
 </head>
 <body>
-	<form>
-		<button type="submit" class="button btn-proceed-checkout" id="buyButton" name="prePago" title="Procesar con el Pago"><span>Procesar con el Pago</span></button>
-	</form>
 
+
+		<button type="submit" class="button btn-proceed-checkout" id="buyButton" name="prePago" title="Procesar con el Pago"><span>Procesar con el Pago</span></button>
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
   <script>
     Culqi.publicKey = 'pk_test_25E7HHJpVTXS26cr';
@@ -84,7 +90,7 @@
           email: '<?php echo $correo_comp; ?>' 
         };
 
-        var url = "../plugins/proceso.php";
+        var url = "./plugins/proceso.php";
 
         $.post(url,data,function(res){
           alert(' Tu pago se Realizó con ' + res + '. Agradecemos tu preferencia.');
